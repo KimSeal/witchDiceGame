1bcfab629ee30fe75ac4ae1c6b3551d0cdabd329
kimseal <tidhtmclwm@naver.com>
2025-07-22 17:46:29 +0900

    talkImage update

--- witchDiceGame/My project (2)/Assets/Script/TalkManager.cs
+++ witchDiceGame/My project (2)/Assets/Script/TalkManager.cs
@@ -249,8 +249,8 @@ public class TalkManager : MonoBehaviour
         //캐릭터 스프라이트 업데이트
         for (int i = 0; i < lightingArr.Length; i++)
         {
-            if (nameArr[i] == "People") characterImage[i].GetComponent<RectTransform>().sizeDelta = new Vector2(200 ,216);
-            else characterImage[i].GetComponent<RectTransform>().sizeDelta = new Vector2(100f, 216f);
+            if (nameArr[i] == "People" || nameArr[i] == "Bard" || nameArr[i] == "Tom" || nameArr[i] == "Bob" ) characterImage[i].GetComponent<RectTransform>().sizeDelta = new Vector2(204f ,216f);
+            else characterImage[i].GetComponent<RectTransform>().sizeDelta = new Vector2(102f, 216f);
 
             if (nameArr[i] == ".") characterImage[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("sprite/TestSprite/CharacterTalkStand/empty/spr_stand_" + nameArr[i] + "_" + faceArr[i]);
             else characterImage[i].GetComponent<Image>().sprite = Resources.Load<Sprite>("sprite/TestSprite/CharacterTalkStand/" + nameArr[i] + "/spr_stand_" + nameArr[i] + "_" + faceArr[i]);
